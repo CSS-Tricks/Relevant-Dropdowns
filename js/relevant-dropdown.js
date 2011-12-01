27430bc15cd6f6213125e3bc751223e12ba3c692
Chris Coyier <chriscoyier@gmail.com>
2011-11-30 20:19:16 -0800

    polyfill instead of two ways

--- js/relevant-dropdown.js
+++ js/relevant-dropdown.js
@@ -1,33 +1,121 @@
+// Make jQuery's :contains case insensitive (like HTML5 datalist)
 jQuery.expr[':'].contains = function(a, i, m) { 
   return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0; 
 };
 
 $(function() {
+	
+	if (!Modernizr.input.list) {
 
-	var doc = $(document);
+		var doc = $(document);
 	
-	var search1 = $("#search-1");
-	var searchPosition1 = search1.position();
-	var datalist1 = $("#datalist-1");
-	var datalist1items = datalist1.find("li");
-	
-	doc.on("keyup", "#search-1", function(e) {
+		var $searchInput = $("#search");
+		var searchPosition = $searchInput.position();
+		var $datalist = $("#" + $searchInput.attr("list"));
+		var datalistItems = $datalist.find("option");
 		
-		datalist1
-				.show()
-				.css({
-					top: searchPosition1.top + $(this).outerHeight(),
-					left: searchPosition1.left,
-					width: search1.outerWidth()
-				})
-				.mouseleave(function() {
-					datalist1.fadeOut();
-				});
+		// Insert home for new fake datalist
+		$("<ul />", {
+			"class": "datalist",
+			"id"   : $searchInput.attr("list")
+		}).appendTo("body");
+		
+		// Remove old datalist
+		$datalist.remove();
+		
+		// Update pointer
+		var $datalist = $("#" + $searchInput.attr("list"));
+		
+		// Fill new fake datalist
+		datalistItems.each(function() {
+			$("<li />", {
+				"text": $(this).text()
+			}).appendTo($datalist);
+		});
+		
+		// Update pointer
+		var datalistItems = $datalist.find("li");
+	
+		// Typey type type
+		doc.on("keyup", "#search", function(e) {
+								
+			$datalist
+					.show()
+					.css({
+						top: searchPosition.top + $(this).outerHeight(),
+						left: searchPosition.left,
+						width: $searchInput.outerWidth()
+					})
+					.mouseleave(function() {
+						$datalist.fadeOut();
+						datalistItems.removeClass("active");
+					});
 				
-		datalist1items.hide();
+			datalistItems.hide();
 		
-		datalist1.find("li:contains('" + search1.val() + "')").show();
+			$datalist.find("li:contains('" + $searchInput.val() + "')").show();
 	
-	});
+		});
+		
+		// Don't want to use :hover in CSS so doing this instead
+		datalistItems.on("mouseenter", function() {
+			$(this).addClass("active");
+		});
+		datalistItems.on("mouseleave", function() {
+			$(this).removeClass("active");
+		});
+	
+		// TODO
+		$(window).resize(function() {
+			// Move it
+		});
+		
+		// Watch arrow keys for up and down
+		doc.on("keydown", function(e) {	
+						
+			var active = $("li.active");
+			
+			// up arrow		
+			if (e.keyCode == 38) {
+				if (active.length) {
+					prevAll = active.prevAll("li:visible");
+					if (prevAll.length > 0) {
+						active.removeClass("active");
+						prevAll.eq(0).addClass("active");
+					}
+				}
+			}
+			// down arrow
+			if (e.keyCode == 40) {
+				if (active.length) {
+					var nextAll = active.nextAll("li:visible");
+					if (nextAll.length > 0) {
+						active.removeClass("active");
+						nextAll.eq(0).addClass("active");
+					}
+				} else {
+					$datalist.find("li:first").addClass("active");
+				}
+			} 
+			// return key
+			if (e.keyCode == 13) {
+				var active = $("li.active");
+				if (active.length) {
+					$searchInput.val(active.text());
+				}
+			}
+			
+		});
+		
+		// When choosing from dropdown
+		// TODO work with return key too
+		datalistItems.on("click", function() {
+			var active = $("li.active");
+			if (active.length) {
+				$searchInput.val($(this).text());
+			}
+		});
+	
+	}
 
 });
