bcb9bc63452a65f03519fdedc6454c7ec93aeea0
Ruan Mer <ruanrm@gmail.com>
2011-12-11 20:31:13 -0200

    Improved code organization

--- js/jquery.relevant-dropdown.js
+++ js/jquery.relevant-dropdown.js
@@ -15,176 +15,195 @@
   $.expr[':'].RD_contains = function(a, i, m) { 
     return $(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0; 
   };
-  
-  if ( !supportDatalist ) {
-    window.relevantDropdown = (function() {
-      
-      $('datalist').each(function() {
+        
+  function RD ( datalist ) {    
+         
+    this.datalistElem   = datalist;
+    this.listID         = this.datalistElem.attr('id');
+    this.inputElem      = $(' input[ list=' + this.listID + ' ] ');
+    this.datalistItems  = this.datalistElem.find('option');
+    
+    this.inputPosition  = 0;
+    this.scrollValue    = 0;
+    
+    var self = this;
 
-        var $datalist = $(this),
-            list_id = $datalist.attr("id"),
-            $input = $("input[list="+ list_id +"]"),
-            datalistItems = $datalist.find("option"),
+    // Used to prevent reflow
+    var temp_items = document.createDocumentFragment();
+    var temp_item = null;
 
-            searchPosition,
-            scrollValue = 0,
+    // Insert home for new fake datalist
+    $("<ul />", {
+      "class": "datalist",
+      "id"   : this.listID
+    }).appendTo("body");
 
-            // Used to prevent reflow
-            temp_items = document.createDocumentFragment(),
-            temp_item = null;
+    // Remove old datalist
+    this.datalistElem.remove();
 
-        // Insert home for new fake datalist
-        $("<ul />", {
-          "class": "datalist",
-          "id"   : list_id
-        }).appendTo("body");
+    // Update pointer
+    this.datalistElem = $("#" + this.listID);
+    
+    // Set datalist CSS        
+    this.setCSS();        
+    
+    // Fill new fake datalist
+    this.datalistItems.each(function() {
+      temp_item = $("<li />", {
+			// .val is required here, not .text or .html
+			// HTML *needs* to be <option value="xxx"> not <option>xxx</option>  (IE)
+        "text": $(this).val()   
+      })[0];
+      temp_items.appendChild(temp_item);
+    });
+    this.datalistElem.append(temp_items);
 
-        // Remove old datalist
-        $datalist.remove();
+    // Update pointer
+    this.datalistItems = this.datalistElem.find('li');
 
-        // Update pointer
-        $datalist = $("#" + list_id);
+    // Typey type type
+    this.inputElem
+      .on('focus', function(){
+        self.resetScroll();
+      })    		
+      .on('blur', function() {
+        // If this fires immediately, setTimeout prevents click-to-select from working
+        setTimeout(function() {
+          self.hideIt();
+        }, 500);
+      })
+      .on('keyup focus', function(e) {
+        // Show datalist							
+        self.datalistElem.show();
+        
+        // Search for
+        self.datalistItems
+          .hide()
+          .filter(':RD_contains("' + self.inputElem.val() + '")').show();    				
+      })
+      // Watch arrow keys for up and down
+      .on('keydown', function(e) {	
 
-        // Fill new fake datalist
-        datalistItems.each(function() {
-          temp_item = $("<li />", {
-  				// .val is required here, not .text or .html
-  				// HTML *needs* to be <option value="xxx"> not <option>xxx</option>  (IE)
-            "text": $(this).val()   
-          })[0];
-          temp_items.appendChild(temp_item);
-        });
-        $datalist.append(temp_items);
+        var active = self.getActive();
+        var datalistHeight = self.datalistElem.outerHeight();
+        var datalistItemsHeight = self.datalistItems.outerHeight();
 
-        // Update pointer
-        datalistItems = $datalist.find("li");
-
-        // Typey type type
-        $input
-          .on("focus", function() {   					
-            // Reset scroll				
-            $datalist.scrollTop(0);    					
-            scrollValue = 0;
-          })    		
-          .on("blur", function() {
-            // If this fires immediately, it prevents click-to-select from working
-            setTimeout(function() {
-              $datalist.fadeOut();
-              datalistItems.removeClass("active"); 
-            }, 500);
-          })
-          .on("keyup focus", function(e) {
-            searchPosition = $input.position();
-            // Build datalist							
-            $datalist
-              .show()
-              .css({
-                top: searchPosition.top + $(this).outerHeight(),
-                left: searchPosition.left,
-                width: $input.outerWidth()
-              });
-
-            datalistItems.hide();
-            $datalist.find("li:RD_contains('" + $input.val() + "')").show();    				
-          });
-
-        // Don't want to use :hover in CSS so doing this instead
-        // really helps with arrow key navigation
-        datalistItems
-          .on("mouseenter", function() {
-            $(this).addClass("active").siblings().removeClass("active");
-          })
-          .on("mouseleave", function() {
-            $(this).removeClass("active");
-          });
-
-        // Window resize
-        $(window).resize(function() {
-          searchPosition = $input.position();
-          $datalist
-            .css({
-              top: searchPosition.top + $(this).outerHeight(),
-              left: searchPosition.left,
-              width: $input.outerWidth()
-            });
-        });		
-
-        // Watch arrow keys for up and down
-        $input.on("keydown", function(e) {	
-
-          var active = $("li.active"),
-              datalistHeight = $datalist.outerHeight(),
-              datalistItemsHeight = datalistItems.outerHeight();
-
-          // up arrow		
-          if ( e.keyCode == 38 ) {
-            if (active.length) {
-              prevAll = active.prevAll("li:visible");
-              if (prevAll.length > 0) {
-                active.removeClass("active");
-                prevAll.eq(0).addClass("active");
-              }            
-
-              if ( prevAll.length && prevAll.position().top < 0 && scrollValue > 0 ){
-                $datalist.scrollTop(scrollValue-=datalistItemsHeight);                        
-              }                    
-            }
-          }
-
-          // down arrow
-          if ( e.keyCode == 40 ) {
-            if (active.length) {
-              var nextAll = active.nextAll("li:visible");
-              if (nextAll.length > 0) {
-                active.removeClass("active");
-                nextAll.eq(0).addClass("active");
-              }                 
-
-              if ( nextAll.length && (nextAll.position().top + datalistItemsHeight) >= datalistHeight ){
-                $datalist.scrollTop(scrollValue+=datalistItemsHeight);
-              }                    
-            } else {			    
-              datalistItems.removeClass("active");
-              $datalist.find("li:visible:first").addClass("active");	
-            }		    
-          }
-
-          // return or tab key
-          if ( e.keyCode == 13 || e.keyCode == 9 ){
-            if (active.length) {
-              $input.val(active.text());
-              item_selected(active.text());
-            }
-            $datalist.fadeOut();
-            datalistItems.removeClass("active");
-          }
-
-          // keys
-          if ( e.keyCode != 13 && e.keyCode != 38 && e.keyCode != 40 ){
-            // Reset active class
-            datalistItems.removeClass("active");
-            $datalist.find("li:visible:first").addClass("active");
-
-            // Reset scroll
-            $datalist.scrollTop(0);	
-            scrollValue = 0;		    
-          }
-
-        });
-
-        // When choosing from dropdown
-        datalistItems.on("click", function() {
-          var active = $("li.active");
+        // up arrow		
+        if ( e.keyCode == 38 ) {
           if (active.length) {
-            $input.val($(this).text());
+            prevAll = active.prevAll("li:visible");
+            if (prevAll.length > 0) {
+              active.removeClass("active");
+              prevAll.eq(0).addClass("active");
+            }            
+
+            if ( prevAll.length && prevAll.position().top < 0 && self.scrollValue > 0 ){
+              self.datalistElem.scrollTop(self.scrollValue-=datalistItemsHeight);                        
+            }                    
           }
-          $datalist.fadeOut();
-          datalistItems.removeClass("active");
-        });
+        }
+
+        // down arrow
+        if ( e.keyCode == 40 ) {
+          if (active.length) {
+            var nextAll = active.nextAll("li:visible");
+            if (nextAll.length > 0) {
+              active.removeClass("active");
+              nextAll.eq(0).addClass("active");
+            }                 
+
+            if ( nextAll.length && (nextAll.position().top + datalistItemsHeight) >= datalistHeight ){
+              self.datalistElem.scrollTop(self.scrollValue+=datalistItemsHeight);
+            }                    
+          } else {			    
+            self.datalistItems.removeClass("active");
+            self.datalistElem.find("li:visible:first").addClass("active");	
+          }		    
+        }
+
+        // return or tab key
+        if ( e.keyCode == 13 || e.keyCode == 9 ){
+          if (active.length) {
+            self.setValue( active.text() );
+          }
+          self.hideIt();
+        }
+
+        // keys
+        if ( e.keyCode != 13 && e.keyCode != 38 && e.keyCode != 40 ){
+          // Reset active class
+          self.datalistItems.removeClass("active");
+          self.datalistElem.find("li:visible:first").addClass("active");
+
+          self.resetScroll();		    
+        }
 
       });
-      
-    })();
-  } 
+
+    // Don't want to use :hover in CSS so doing this instead
+    // really helps with arrow key navigation
+    this.datalistItems
+      .on('mouseenter', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+      })
+      .on('mouseleave', function() {
+        $(this).removeClass('active');
+      })
+      // When choosing from dropdown
+      .on("click", function() {
+       var active = self.getActive();
+       
+        if (active.length) {
+          self.setValue( $(this).text() );
+        }
+        self.hideIt();
+      });
+
+    // Window resize
+    $(window).resize(function() {   
+      self.setCSS();
+    });		
+
+  }// end RD
+
+  // Set datalist CSS
+  RD.prototype.setCSS = function () { 
+    this.datalistElem.css({
+      top: this.inputElem.position().top + this.inputElem.outerHeight(),
+      left: this.inputElem.position().left,
+      width: this.inputElem.outerWidth()
+    });
+  }
+  
+  // Get active element
+  RD.prototype.getActive = function() {
+    return this.datalistElem.find('.active');
+  }
+  
+  RD.prototype.setValue = function( value ) {
+    this.inputElem.val( value );
+  }
+  
+  // Hide datalist
+  RD.prototype.hideIt = function () { 
+    this.datalistElem.fadeOut();
+    this.datalistItems.removeClass('active'); 
+  }
+
+  // Reset datalist scroll
+  RD.prototype.resetScroll = function () {
+    this.datalistElem.scrollTop(0);    					
+    this.scrollValue = 0;        
+  }
+  
+  // If don't support datalist
+  if ( !supportDatalist ) {
+    // Each <datalist>
+    $('datalist').each(function() {        
+      if (!$.data(this, 'relevantDropdown')) {
+          $.data(this, 'relevantDropdown', new RD( $(this) ));
+      }
+    });
+  } // endif
   
 })(jQuery);
